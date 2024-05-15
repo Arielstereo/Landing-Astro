768a9576a94add758958800b966a6b52674df6e2
Ariel Martinez <arielstereo@msn.com>
2024-05-15 17:15:57 -0300

    add price in cards

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,19 +1,17 @@
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+import AOS from "aos";
+import "aos/dist/aos.css";
 import { useEffect } from "react";
 
-
-const ProductCard = ({ title, description, img, animation }) => {
-  
+const ProductCard = ({ title, description, img, animation, price }) => {
   useEffect(() => {
     AOS.init({
-      duration: 1000
+      duration: 1000,
     });
   }, []);
   return (
     <div
       data-aos={animation}
-      className="p-6 flex flex-col bg-slate-100 mx-auto gap-2 shadow-lg shadow-black rounded-md"
+      className="p-6 flex flex-col bg-slate-100 mx-auto gap-2 shadow-lg shadow-black rounded-md relative"
     >
       <div className="bg-slate-800 w-full py-2 rounded-lg pl-4">
         <h2 className="text-xl text-slate-100 font-semibold">{title}</h2>
@@ -22,15 +20,12 @@ const ProductCard = ({ title, description, img, animation }) => {
         className="hover:grow hover:shadow-xl hover:grayscale object-cover w-[300px] h-[250px] rounded-xl"
         src={img}
       />
+      <div className="text-lg text-slate-800 bg-yellow-400 hover:bg-yellow-300 text-center p-4 w-24 h-24 rounded-full font-medium absolute right-6 bottom-48">
+        Desde ${price}
+      </div>
       <div className="w-[300px] h-24 py-2">
         <span className="text-sm text-slate-500">{description}</span>
       </div>
-      <a
-        href="#contact"
-        className="text-lg text-slate-100 bg-pink-400 hover:bg-pink-300 w-full text-center p-2 rounded-md font-medium"
-      >
-        Consultar
-      </a>
     </div>
   );
 };
